a37ce6b8d445665c01bb0a0782c5ee32cc952fa0
Cristian Adán <hola@salteadorneo.dev>
2023-05-26 01:20:27 +0200

    add vitest + first tests

---
+++ src/test/core.test.js
@@ -0,0 +1,65 @@
+import { describe, expect, it } from 'vitest'
+import { getResult } from '../core'
+import { DEFAULT_VALUE } from '../consts'
+
+describe('test code results', () => {
+  it('console.log', async () => {
+    const output = await getResult(DEFAULT_VALUE)
+    const expected = `
+
+
+
+'👋🌎'`
+    expect(output).eq(expected)
+  })
+
+  it('await fetch', async () => {
+    const output = await getResult(`async function getFetch() {
+    return await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.json())
+}
+
+getFetch()`)
+    const expected = `
+
+
+
+
+{"userId":1,"id":1,"title":"delectus aut autem","completed":false}`
+    expect(output).eq(expected)
+  })
+
+  it('import from', async () => {
+    const output = await getResult(`import confetti from 'canvas-confetti'
+
+function getParty() {
+    confetti({
+        particleCount: 100,
+        startVelocity: 30,
+        spread: 360,
+        origin: {
+            x: Math.random(),
+            y: Math.random() - 0.2
+        }
+    });
+    setTimeout(getParty, 2000)
+}
+getParty()`)
+    const expected = `
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+    expect(output).eq(expected)
+  })
+})
