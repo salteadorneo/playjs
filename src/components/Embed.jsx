38fa961bc0b6e6ebb51de02968f6d3be67b22cd0
Cristian Adán <hola@salteadorneo.dev>
2023-05-25 02:01:48 +0200

    fix embed copy

--- src/components/Embed.jsx
+++ src/components/Embed.jsx
@@ -1,20 +1,20 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import Button from './Button'
 import { toast } from 'sonner'
 import { IconEmbed } from '../Icons'
 
 export default function Embed () {
-  const textareaRef = useRef(null)
   const [modal, setModal] = useState(false)
 
   const url = window.location.href
+  const iframe = `<iframe src="${url}" width="100%" height="500" style="border:none;"></iframe>`
 
   function handleClick () {
     setModal(status => !status)
   }
 
   function handleCopy () {
-    navigator.clipboard.writeText(textareaRef.current.value)
+    navigator.clipboard.writeText(iframe)
 
     toast.success('Copied to clipboard!')
 
@@ -37,9 +37,7 @@ export default function Embed () {
         <section className='modal fixed top-0 left-0 z-20 w-screen h-screen bg-black/80 grid place-content-center'>
           <div className='max-w-xl w-screen px-4 space-y-2 text-right'>
             <textarea
-              ref={textareaRef}
-              value={`<iframe src="${url}" width="100%" height="100%" style="border: none;"></iframe>`}
-              onClick={handleCopy}
+              value={iframe}
               className='w-full h-48 bg-background text-primary border-none outline-none resize-none p-4'
               readOnly
             />
