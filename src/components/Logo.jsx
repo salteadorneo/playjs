d29644c43a74c6bd1703610cb0553bea488f8ed9
Logan Benavente <hello.piguicorn@gmail.com>
2023-06-18 15:27:04 +0200

    shows warning message when user reaches the amount of data we are able to store online

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,7 +8,7 @@ export default function Logo () {
       : children
 
   return (
-    <div className='fixed top-0 z-10 w-full flex items-center gap-3 p-3 shadow-sm bg-[#1a1a1a]'>
+    <>
       <Wrapper>
         <div className={`w-6 h-6 flex items-center justify-center ${language === 'typescript' ? 'bg-typescript' : 'bg-secondary'}`} />
         <h1 className='text-white text-2xl font-extrabold'>
@@ -19,6 +19,6 @@ export default function Logo () {
         <span>v.{version}</span>
         <span className='text-[#1a1a1a] font-bold bg-[#3f3f3f] rounded py-[1px] px-2'>BETA</span>
       </span>
-    </div>
+    </>
   )
 }
