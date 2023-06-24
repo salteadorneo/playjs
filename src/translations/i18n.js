aa0701ffaa6a8e63034bbdf5b6ea393d14667853
Lautaro Suarez <67484166+lausuarez02@users.noreply.github.com>
2023-06-24 22:41:38 +0200

    Fix i18 file and Add json files to be more readable :) (#49)

--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -1,6 +1,9 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
+//Jsons
+import LanguageESJSON from './language.es.json'
+import LanguageENJSON from './language.en.json'
 
 i18n
   .use(LanguageDetector)
@@ -12,110 +15,8 @@ i18n
       escapeValue: false,
     },
     resources: {
-      en: {
-        translation: {
-          code: {
-            formatTitle: 'Format code',
-            format: 'Format',
-            downloadTitle: 'Download code as file',
-            download: 'Download',
-          },
-          displayOptions: {
-            displayTitle: 'Change view',
-            display: 'View'
-          },
-          embed: {
-            toast: 'Copied to clipboard!',
-            embedTitle: 'Get HTML',
-            embed: 'Embed',
-            embedClose: 'Close',
-            embedCopy: 'Copy',
-          },
-          language: {
-            globeTitle: 'Change language',
-            toast: 'Language changed!',
-          },
-          report: {
-            responseOk: 'Thanks for your report!',
-            responseError: 'Oops! There was a problem submitting your form',
-            reportTitle: 'Report a bug',
-            report: 'Report bug',
-            formEmail: 'Email (optional)',
-            formDescription: 'Description',
-            formCancel: 'Cancel',
-            formSend: 'Send',
-            reportClose: 'Close',
-          },
-          share: {
-            toast: 'Copied to clipboard!',
-            shareTitle: 'Share code',
-            share: 'Share',
-          },
-          upload: {
-            toast: 'File uploaded!',
-            uploadTitle: 'Upload code from file',
-            upload: 'Upload',
-            instructions: 'Select or drop a file below',
-            formCancel: 'Cancel',
-            formUpload: 'Upload',
-          },
-          urlLengthError: {
-            warning: '⚠️ You reached the limit of data we can store online. You may want to download your code so recent changes don\'t get lost.',
-          },
-        }
-      },
-      es: {
-        translation: {
-          code: {
-            formatTitle: 'Dar formato',
-            format: 'Formato',
-            downloadTitle: 'Descargar código en fichero',
-            download: 'Descargar',
-          },
-          displayOptions: {
-            displayTitle: 'Cambiar vista',
-            display: 'Vista'
-          },
-          embed: {
-            toast: 'Copiado al portapapeles!',
-            embedTitle: 'Obtener HTML',
-            embed: 'Insertar',
-            embedClose: 'Cerrar',
-            embedCopy: 'Copiar',
-          },
-          language: {
-            globeTitle: 'Cambiar idioma',
-            toast: 'Idioma cambiado!',
-          },
-          report: {
-            responseOk: 'Gracias por tu reporte!',
-            responseError: 'Ups! Ha habido un problema enviando el formulario',
-            reportTitle: 'Reportar error',
-            report: 'Reportar error',
-            formEmail: 'Email (opcional)',
-            formDescription: 'Descripción',
-            formCancel: 'Cancelar',
-            formSend: 'Enviar',
-            reportClose: 'Cerrar',
-          },
-          share: {
-            toast: 'Copiado al portapapeles!',
-            shareTitle: 'Compartir código',
-            share: 'Compartir',
-          },
-          upload: {
-            toast: 'Fichero subido!',
-            uploadTitle: 'Subir código de fichero',
-            upload: 'Subir',
-            instructions: 'Selecciona o arrastra un fichero',
-            formCancel: 'Cancelar',
-            formUpload: 'Subir',
-          },
-          urlLengthError: {
-            warning: '⚠️ Has llegado al límite de código que podemos almacenar en línea. Puedes descargarlo para no perder los últimos cambios.',
-          },
-        }
-      }
+      en: LanguageENJSON,
+      es: LanguageESJSON
     }
   })
 
