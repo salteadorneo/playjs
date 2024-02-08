214563b5037692656e4c0f6499143fc199b5a97a
Cristian Adán <hola@salteadorneo.dev>
2024-02-09 00:29:38 +0100

    Remove js-base64

--- src/core/encode.js
+++ src/core/encode.js
@@ -1,11 +1,9 @@
-import { encode, decode } from 'js-base64'
-
 export function encodeCode (code) {
-  return encode(code)
+  return bytesToBase64(new TextEncoder().encode(code))
 }
 
 export function decodeCode (code) {
-  return decode(code)
+  return new TextDecoder().decode(base64ToBytes(code))
 }
 
 export function getCodeFromURL () {
@@ -13,8 +11,18 @@ export function getCodeFromURL () {
     const { pathname } = window.location
     if (pathname.length <= 1) return false
     const hashCode = pathname.slice(1)
-    return hashCode ? decode(hashCode) : false
+    return hashCode ? decodeCode(hashCode) : false
   } catch {
     return false
   }
+}
+
+function base64ToBytes (base64) {
+  const binString = atob(base64)
+  return Uint8Array.from(binString, (m) => m.codePointAt(0))
+}
+
+function bytesToBase64 (bytes) {
+  const binString = Array.from(bytes, (x) => String.fromCodePoint(x)).join('')
+  return btoa(binString)
 }
