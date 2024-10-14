dec5ade84b03de5e9bd85c58364c1fa9dae6d3fb
Cristian Adán <hola@salteadorneo.dev>
2024-10-15 01:34:03 +0200

    Refactor Vite configuration to add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [deno(), react()],
+  base: '/playjs/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
