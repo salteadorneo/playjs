12f344bc5259a03ae148db52e95f13d808983e28
Cristian Adán <hola@salteadorneo.dev>
2024-10-15 21:45:06 +0200

    Refactor main

--- packages/playjs-core/src/main.tsx
+++ packages/playjs-core/src/main.tsx
@@ -2,13 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const root = document.getElementById('root')
-
-if (!root) {
-  throw new Error('Root element not found')
-}
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
