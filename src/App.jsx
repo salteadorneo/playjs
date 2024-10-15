5d81d93ce8346cda40cabf60d9b771e7e0e4cea3
Cristian Adán <hola@salteadorneo.dev>
2024-10-15 21:44:23 +0200

    Refactor App component to always render Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import Main from './components/Main'
-import { getHashFromURL } from './core/encode'
-import { useCodeStore } from './hooks/useCodeStore'
-import Landing from './pages/Landing'
 
 export default function App () {
-  const { codes } = useCodeStore()
-
-  const url = new URL(window.location.href)
-  const utmSource = url.searchParams.get('utm_source')
-  if (utmSource === 'homescreen' || getHashFromURL() || codes.length > 1) {
-    return <Main />
-  }
-
-  return <Landing />
+  return <Main />
 }
