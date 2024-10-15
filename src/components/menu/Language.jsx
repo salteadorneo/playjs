0a2d72b5a101b3524fd16f4c52491b49fdfd01e9
Cristian Adán <hola@salteadorneo.dev>
2024-10-16 01:42:16 +0200

    Refactor Language component styles for language selection

--- src/components/menu/Language.jsx
+++ src/components/menu/Language.jsx
@@ -9,29 +9,12 @@ export default function Language () {
   }
 
   return (
-    <div className='relative flex items-center gap-2 bg-none border-none text-[#858585] hover:text-[#999] hover:bg-[#252525] p-1 cursor-default'>
-      <svg
-        width={18}
-        height={18}
-        viewBox='0 0 24 24'
-        xmlns='http://www.w3.org/2000/svg'
-        aria-labelledby='languageIconTitle'
-        stroke='currentColor'
-        strokeLinecap='square'
-        fill='none'
-        className='absolute top-1/2 left-1 -translate-y-1/2'
-      >
-        <circle cx={12} cy={12} r={10} />
-        <path
-          strokeLinecap='round'
-          d='M12 22c2.667-2.424 4-5.758 4-10s-1.333-7.576-4-10C9.333 4.424 8 7.758 8 12s1.333 7.576 4 10ZM2.5 9h19m-19 6h19'
-        />
-      </svg>
+    <div className='relative flex items-center gap-2 bg-none border border-[#858585] rounded-md text-[#858585] hover:text-[#999] hover:bg-[#252525] p-1.5 cursor-default'>
       <select
         value={i18n.resolvedLanguage}
         onChange={changeLanguage}
         title={t('language.globeTitle')}
-        className='pl-[22px] w-full text-[#858585] bg-transparent rounded-md outline-none'
+        className='w-full text-[#858585] bg-transparent outline-none'
       >
         <option value='en' className='bg-[#1a1a1a]'>English</option>
         <option value='es' className='bg-[#1a1a1a]'>Español</option>
