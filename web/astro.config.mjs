5b8a6738cd370a586235ee5b382d7c7f2945e328
Cristian Adán <hola@salteadorneo.dev>
2024-10-18 01:12:06 +0200

    Refactor Astro configuration and package.json scripts

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config'
-import deno from '@deno/astro-adapter';
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: deno(),
 })
