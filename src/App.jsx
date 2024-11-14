bfc71d5585581cf86d9a2be833dc2efad8e3b980
Cristian Adán <hola@salteadorneo.dev>
2024-11-14 01:27:26 +0100

    Refactor App component to conditionally render UI elements based on IS_IFRAME

--- src/App.jsx
+++ src/App.jsx
@@ -66,28 +66,30 @@ export default function App () {
       <Toaster position='top-center' />
 
       <main className='flex'>
-        <div className='relative z-10 shadow-sm bg-[#1a1a1a]'>
-          <div className='flex flex-col justify-between h-full'>
-            <Menu
-              theme={theme}
-              setTheme={setTheme}
-              language={language}
-              setLanguage={setLanguage}
-              setCode={setCode}
-            />
+        {!IS_IFRAME && (
+          <div className='relative z-10 shadow-sm bg-[#1a1a1a]'>
+            <div className='flex flex-col justify-between h-full'>
+              <Menu
+                theme={theme}
+                setTheme={setTheme}
+                language={language}
+                setLanguage={setLanguage}
+                setCode={setCode}
+              />
 
-            <div className='flex flex-col items-center gap-4'>
-              <Button
-                onClick={changeDirection}
-                title={t('displayOptions.displayTitle')}
-              >
-                <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className={`${direction === DIRECTION.VERTICAL ? 'rotate-90' : ''}`}><rect width='18' height='18' x='3' y='3' rx='2' /><path d='M3 12h18' /></svg>
-              </Button>
-              <Share />
-              <Embed />
+              <div className='flex flex-col items-center gap-4'>
+                <Button
+                  onClick={changeDirection}
+                  title={t('displayOptions.displayTitle')}
+                >
+                  <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className={`${direction === DIRECTION.VERTICAL ? 'rotate-90' : ''}`}><rect width='18' height='18' x='3' y='3' rx='2' /><path d='M3 12h18' /></svg>
+                </Button>
+                <Share />
+                <Embed />
+              </div>
             </div>
           </div>
-        </div>
+        )}
 
         <div className='flex flex-col'>
           {!IS_IFRAME && <Tabs />}
@@ -98,8 +100,8 @@ export default function App () {
             direction={direction}
             language={language || LANGUAGE.JAVASCRIPT}
             theme={theme}
-            width='calc(100dvw - 50px)'
-            height={IS_IFRAME ? '100dvh' : 'calc(100dvh - 40px)'}
+            width={!IS_IFRAME ? 'calc(100dvw - 50px)' : '100vw'}
+            height={!IS_IFRAME ? 'calc(100dvh - 40px)' : '100dvh'}
             ia
           />
         </div>
