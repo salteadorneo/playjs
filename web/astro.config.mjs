358d45fdbd79f6d6cc0890bd1f52f16798f1675a
Cristian Adán <cristian@salteadorneo.dev>
2026-01-31 01:14:06 +0100

    fix: update site URL in astro.config.mjs

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  site: 'https://web.playjs.dev',
+  site: 'https://salteadorneo.github.io',
+  base: '/playjs-web',
   integrations: [react(), tailwind()],
 })
