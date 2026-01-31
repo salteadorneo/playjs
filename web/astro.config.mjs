90d1ed5645b278ea6eb1260137eb862f852988ad
Cristian Adán <cristian@salteadorneo.dev>
2026-01-31 01:07:15 +0100

    Remove CNAME from web/public

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,5 +3,6 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
+  site: 'https://web.playjs.dev',
   integrations: [react(), tailwind()],
 })
